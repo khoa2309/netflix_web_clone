3e7d14f04e7e220833bbe29d09060dda2666ce80
Khoa2309 <emkhoa239@gmail.com>
2023-09-19 23:47:15 +0700

    update

--- src/routes/ScrollToTop.js
+++ src/routes/ScrollToTop.js
@@ -1,17 +1,15 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
-
   useEffect(() => {
-    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
-    document.documentElement.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "instant", // Optional if you want to skip the scrolling animation
-    });
-  }, [pathname]);
+    const handleUnload = () => {
+      window.scrollTo(0, 0);
+    };
+    window.addEventListener("unload", handleUnload);
+    return () => {
+      window.removeEventListener("unload", handleUnload);
+    };
+  }, []);
 
   return null;
 }
